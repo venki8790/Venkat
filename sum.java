b624737cd711799e2343284217d4aea3d2b6ea77
venki8790 <rvreddy2145@gmail.com>
2024-05-10 09:54:12 +0530

    sum.java

---
+++ sum.java
@@ -0,0 +1,31 @@
+
+class Sum
+{
+public static void main(String args[])
+{
+int num1, num2, res, a1, a2;
+Sysstem. out.println("Enter first number value");
+Scanner s = new Scanner(System. in) ;
+num1=s. nextInt ();
+a1=25;
+a2=45;
+Sysstem. out. println("Enter second number value");
+num2=s . nextInt () ;
+res=(num1+num2)/num2;
+System. out. println("This program is to find sum of two integer values");
+System. out. println("Result is "+res);
+if(res>0)
+System. out. println("Result is Positive number");
+
+SYstem. out. println("Results is negative number");
+System. out. println("This is extra line added");
+System. out. println ("This is end of the program");
+System. out. println ("Thank you DEVOPS TEAM") ;
+System. out. println("Todays date is 4th Feb");
+//End of the main method
+
+else
+
+}
+}
+
